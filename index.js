94608341d955d77d34d54f70068b69d433673a91
Nazar Kruk <nazarkruk@icloud.com>
2022-07-25 23:43:22 -0400

    commit tree 326c8ed61042f3cc2e1291b8e7b2025eee992bd6

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your Circle class here
+class Circle {
+
+    constructor(radius){
+        this.radius = radius
+    }
+
+
+
+    get diameter(){
+        return this.radius * 2
+    }
+    set diameter(newDiameter){
+        this.radius = newDiameter/2
+    }
+    
+
+
+    get circumference(){
+        return this.diameter * Math.PI
+    }
+
+    set circumference(newCircumference){
+        this.radius = (newCircumference/Math.PI)/2
+    }
+
+
+    get area(){
+        return Math.PI * (this.radius * this.radius)
+    }
+    set area(newArea) {
+        this.radius = Math.sqrt(newArea/Math.PI);
+      }
+
+
+}
